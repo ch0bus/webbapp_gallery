c24a3dec3931230b62110be9c17bc0539dae2828
ch0bus <chobussuboch@gmail.com>
2021-12-14 19:08:13 +0300

    adaptive for mi

--- runapp.py
+++ runapp.py
@@ -10,12 +10,16 @@ Bootstrap(app)
 @app.route('/index')
 def entry_page() -> 'html':
     """Display this webapp's HTML form."""
-    return render_template('index.html',
-                           the_title='Welcome to search4letters on the web!')
+    return render_template('index.html')
 
 @app.route('/single')
 def index() -> 'html':
     return render_template('single.html',)
+
+@app.route('/p_velo')
+def page_velo() -> 'html':
+    return render_template('p_velo.html',)
+
 
 @app.route('/index')
 def backhome() -> 'html':
